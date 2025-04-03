2ac5979286c81aae5adbd126fe18d82a66da2dad
gonzalorportfolio <gromeroeducation@gmail.com>
2025-04-03 15:32:25 -0400

    done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 /////////////////////
 // Imports
 /////////////////////
+const dotenv = require('dotenv');
+dotenv.config();
 
 const express = require('express');
 const path = require('path');
@@ -24,9 +26,22 @@ const logRoutes = (req, res, next) => {
 
 const serveStatic = express.static(pathToDistFolder);
 
+
 // First, we make a controller
 const serveTopArtStories = async (req, res, next) => {
+  const url = `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${process.env.API_KEY}`;
+  try {
+    // This is pretty standard fetching logic
+    const response = await fetch(url);
+    const data = await response.json();
+    const storiesWithTitle = data.results.filter(story => story.title);
 
+    // send the fetched data to the client
+    res.send(storiesWithTitle);
+  } catch (error) {
+    // or send an error. 503 means the service is unavailable
+    res.status(503).send(error);
+  }
 }
 
 ////////////////////////
@@ -37,6 +52,8 @@ app.use(logRoutes);
 app.use(serveStatic);
 
 // GET /api/top-arts-stories
+
+app.get('/api/top-arts-stories', serveTopArtStories);
 
 const port = 8080;
 app.listen(port, () => {
